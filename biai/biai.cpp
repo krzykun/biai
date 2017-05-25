5ac4e673e16e8da88b4f0b8672b6c610c71033ac
Krzysztof Kundera <krzykun358@student.polsl.pl>
2017-05-25 13:29:49 +0200

    First iteration of the project

---
+++ biai/biai.cpp
@@ -0,0 +1,16 @@
+// biai.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+
+int main()
+{
+    return 0;
+}
+
