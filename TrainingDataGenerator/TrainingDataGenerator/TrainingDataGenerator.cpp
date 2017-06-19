7fee674448127071401bf7dbf56d9efceb82f0a5
Sebastian Oprzędek (sebaopr806) <sebaopr806@student.polsl.pl>
2017-06-19 20:40:07 +0200

    XY coordinates test on linear function based on parameter t

--- TrainingDataGenerator/TrainingDataGenerator/TrainingDataGenerator.cpp
+++ TrainingDataGenerator/TrainingDataGenerator/TrainingDataGenerator.cpp
@@ -16,7 +16,7 @@ int main()
 	ofstream file;
 	file.open("trainingData.txt");
 
-	generateData whatShouldIGenerate = Ycoordinate;
+	generateData whatShouldIGenerate = XYcoordinates;
 
 
 	if (whatShouldIGenerate == XOR) {
@@ -30,7 +30,16 @@ int main()
 		}
 	}
 	else if (whatShouldIGenerate == XYcoordinates) {
-
+		file << "topology: 8 8 8 2" << endl;
+		double a = rand() / double(RAND_MAX);
+		double b = rand() / double(RAND_MAX);
+		for (int i = 2000; i >= 0; --i) {
+			double t = rand() / double(RAND_MAX);
+			double delta_t = rand() / double(RAND_MAX) / 10;
+			file << "in:" << " " << a*t << " " << b*t << " " << a*(t+delta_t) << " " << b*(t + delta_t) << " " << a*(t + 2*delta_t) << " " << b*(t + 2*delta_t);
+			file << " " << a*(t + 3*delta_t) << " " << b*(t + 3 * delta_t) << " " << a*(t + 4 * delta_t) << " " << b*(t + 4 * delta_t);
+			file << endl << "out: " << a*(t + 5 * delta_t) << " " << b*(t + 5 * delta_t) << "" << endl;
+		}
 	}
 	else if (whatShouldIGenerate == Ycoordinate) {
 		file << "topology: 5 4 4 1" << endl;
